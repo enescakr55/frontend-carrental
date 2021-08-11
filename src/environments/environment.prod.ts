fe527531fc46732b34c1e5c25f74d339616d9401
enescakr55 <enescakr01@hotmail.com>
2021-08-11 18:34:38 +0300

    11.08.2021 -- Bilgisayardaki dosyalar ile senkronizasyon yapıldı.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: "https://localhost:44387/api/",
+  mainLink: "https://localhost:44387",
   formnotvalidmessage : "Lütfen girdiğiniz verileri kontrol ediniz",
   formnotvalidtitle : "Form Hatası",
 };
